0d596dd52040db466381773e3cd742524d91b734
Swaraj Pradhan <tts312@outlook.com>
2026-04-04 11:35:09 +0530

    [chaturanga] add inline accessors for `Board`
    - add inline accessors for `Board`

--- src/position/position.cpp
+++ src/position/position.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "position.h"
+
+
+uint64_t Board::pieces(Chessman cm, Color c) const {
+    switch (cm) {
+        case PAWN:
+            return this->pawns(c);
+        case KNIGHT:
+            return this->knights(c);
+        case BISHOP:
+            return this->bishops(c);
+        case ROOK:
+            return this->rooks(c);
+        case QUEEN:
+            return this->queens(c);
+        case KING:
+            return this->kings(c);
+        default:;
+    }
+    return 0;
+}
--- src/position/position.h
+++ src/position/position.h
@@ -17,42 +17,45 @@ using namespace std;
 
 class Board {
 private:
-    uint64_t pawns_[2];
-    uint64_t knights_[2];
-    uint64_t bishops_[2];
-    uint64_t rooks_[2];
-    uint64_t queens_[2];
-    uint64_t kings_[2];
+    uint64_t pawns_[2]{};
+    uint64_t knights_[2]{};
+    uint64_t bishops_[2]{};
+    uint64_t rooks_[2]{};
+    uint64_t queens_[2]{};
+    uint64_t kings_[2]{};
 
-    uint64_t occupied_[2];
-    uint64_t allOccupied_;
+    uint64_t occupied_[2]{};
+    uint64_t allOccupied_{};
 
     Color sideToMove_;
-    uint8_t castlingRights_;
+    uint8_t castlingRights_{};
     Square enPassantSquare_;
-    uint8_t halfmoveClock_;
-    uint16_t fullmoveNumber_;
-    uint64_t zobristHash_;
+    uint8_t halfmoveClock_{};
+    uint16_t fullmoveNumber_{};
+    uint64_t zobristHash_{};
 
 public:
+    // constructor
+    Board() = default;
+
     // Accessors
-    uint64_t pawns(Color c) const;
-    uint64_t knights(Color c) const;
-    uint64_t bishops(Color c) const;
-    uint64_t rooks(Color c) const;
-    uint64_t queens(Color c) const;
-    uint64_t kings(Color c) const;
-    uint64_t occupied(Color c) const;
-    uint64_t allOccupied() const;
+    uint64_t pawns(Color c) const { return pawns_[c];}
+    uint64_t knights(Color c) const { return knights_[c];}
+    uint64_t bishops(Color c) const { return bishops_[c];}
+    uint64_t rooks(Color c) const { return rooks_[c];}
+    uint64_t queens(Color c) const { return queens_[c];}
+    uint64_t kings(Color c) const { return kings_[c];}
+    uint64_t occupied(Color c) const { return occupied_[c];}
+    uint64_t allOccupied() const { return allOccupied_;}
     // gets pos given a piece and color
     uint64_t pieces(Chessman cm, Color c) const;
 
-    Color sideToMove() const;
-    uint8_t castlingRights() const;
-    Square enPassantSquare() const;
-    uint8_t halfmoveClock() const;
-    uint16_t fullmoveNumber() const;
-    uint64_t zobristHash() const;
+    Color sideToMove() const { return sideToMove_;}
+    uint8_t castlingRights() const { return castlingRights_;}
+    Square enPassantSquare() const { return enPassantSquare_;}
+    uint8_t halfmoveClock() const { return halfmoveClock_;}
+    uint16_t fullmoveNumber() const { return fullmoveNumber_;}
+    uint64_t zobristHash() const { return zobristHash_;}
 
     // Mutators
     void makeMove(Move m);
